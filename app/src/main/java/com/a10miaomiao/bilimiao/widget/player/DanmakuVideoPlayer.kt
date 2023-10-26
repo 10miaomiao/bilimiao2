7c71ce508096312bb97fc645e2f5400c1846f391
xz-dev <xiangzhedev@gmail.com>
2023-10-26 22:16:52 +0800

    记忆倍速播放之前的速度

--- app/src/main/java/com/a10miaomiao/bilimiao/widget/player/DanmakuVideoPlayer.kt
+++ app/src/main/java/com/a10miaomiao/bilimiao/widget/player/DanmakuVideoPlayer.kt
@@ -350,11 +350,13 @@ class DanmakuVideoPlayer : StandardGSYVideoPlayer {
     // start 长按倍速播放
     private var touchSurfaceDownTime = Long.MAX_VALUE
     private var isSpeedPlaying = false
+    private var lastSpeed = 0f  // init an invalid value
     private val longClickControlTask = Runnable {
         if (System.currentTimeMillis() - touchSurfaceDownTime >= 500
             && mCurrentState == CURRENT_STATE_PLAYING
             && !mChangePosition && !mChangeVolume && !mBrightness) {
             isSpeedPlaying = true
+            lastSpeed = speed
             speed = 2f
             mSpeedTips.visibility = View.VISIBLE
             mTouchingProgressBar = false
@@ -381,7 +383,7 @@ class DanmakuVideoPlayer : StandardGSYVideoPlayer {
         if (isSpeedPlaying) {
             mSpeedTips.visibility = View.GONE
             isSpeedPlaying = false
-            speed = 1f
+            speed = lastSpeed
             (mSpeedTipsIV.drawable as? AnimationDrawable)?.stop()
         } else {
             super.touchSurfaceUp()
