009546da70789de638ce27241ecc664da69e0a49
10miaomiao <1033607264@qq.com>
2023-11-30 23:06:24 +0800

    增加显示及隐藏底部进度条选项

--- bilimiao-comm/src/main/java/com/a10miaomiao/bilimiao/comm/delegate/player/BasePlayerDelegate.kt
+++ bilimiao-comm/src/main/java/com/a10miaomiao/bilimiao/comm/delegate/player/BasePlayerDelegate.kt
@@ -11,6 +11,7 @@ interface BasePlayerDelegate: BaseDelegate {
     fun openPlayer(source: BasePlayerSource)
     fun closePlayer()
     fun updateDanmukuSetting()
+    fun updateVideoSetting()
     fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration?)
     fun isPlaying(): Boolean
     fun setWindowInsets(left: Int, top: Int, right: Int, bottom: Int, displayCutout: DisplayCutout?)
