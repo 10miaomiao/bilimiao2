f2d76b9222e67b04dc5a01610a7c08aeec9688c9
10miaomiao <1033607264@qq.com>
2024-02-09 03:31:08 +0800

    发布版本 2.3.3

--- benchmark/src/main/java/cn/a10miaomiao/bilimiao/benchmark/BaselineProfileGenerator.kt
+++ benchmark/src/main/java/cn/a10miaomiao/bilimiao/benchmark/BaselineProfileGenerator.kt
@@ -1,5 +1,7 @@
 package cn.a10miaomiao.bilimiao.benchmark
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -7,12 +9,14 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@RequiresApi(Build.VERSION_CODES.P)
 @RunWith(AndroidJUnit4::class)
 @OptIn(ExperimentalBaselineProfilesApi::class)
 class BaselineProfileGenerator {
 
     @get:Rule
     val rule = BaselineProfileRule()
+
 
     @Test
     fun generate() {
