889ece636a068ceaa4fb9d1cb863490839e4e146
iHsin <itsusinn@foxmail.com>
2024-02-25 13:35:10 +0000

    feat: 支持标签屏蔽

--- app/src/main/java/com/a10miaomiao/bilimiao/page/home/RecommendViewModel.kt
+++ app/src/main/java/com/a10miaomiao/bilimiao/page/home/RecommendViewModel.kt
@@ -5,11 +5,13 @@ import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import bilibili.app.card.v1.CardOuterClass
 import com.a10miaomiao.bilimiao.comm.MiaoBindingUi
 import com.a10miaomiao.bilimiao.comm.entity.ResultInfo
 import com.a10miaomiao.bilimiao.comm.entity.comm.PaginationInfo
 import com.a10miaomiao.bilimiao.comm.entity.home.HomeRecommendInfo
 import com.a10miaomiao.bilimiao.comm.entity.home.RecommendCardInfo
+import com.a10miaomiao.bilimiao.comm.entity.video.VideoInfo
 import com.a10miaomiao.bilimiao.comm.network.BiliApiService
 import com.a10miaomiao.bilimiao.comm.network.MiaoHttp.Companion.gson
 import com.a10miaomiao.bilimiao.comm.store.FilterStore
@@ -21,6 +23,7 @@ import org.kodein.di.DI
 import org.kodein.di.DIAware
 import org.kodein.di.instance
 import splitties.toast.toast
+import java.util.Locale
 
 class RecommendViewModel(
     override val di: DI,
@@ -63,9 +66,15 @@ class RecommendViewModel(
             if (res.isSuccess) {
                 val itemsList = res.data.items
                 val filterList = itemsList.filter {
-                    (it.goto?.isNotEmpty() ?: false)
+                    var notHide = (it.goto?.isNotEmpty() ?: false)
                             && filterStore.filterWord(it.title)
                             && filterStore.filterUpper(it.args.up_id ?: "-1")
+                    if (filterStore.filterTagCount != 0) {
+                        val tag  = BiliApiService.videoAPI.info(it.param, it.card_goto.uppercase(
+                            Locale.ROOT)).call().gson<ResultInfo<VideoInfo>>().data.tag.joinToString { tag -> tag.tag_name }
+                        notHide = notHide && filterStore.filterWord(tag)
+                    }
+                    notHide
                 }
                 ui.setState {
                     if (idx == 0L) {
---
+++ bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/pages/filter/FilterTagListPage.kt
@@ -0,0 +1,300 @@
+package cn.a10miaomiao.bilimiao.compose.pages.filter
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import cn.a10miaomiao.bilimiao.compose.comm.diViewModel
+import com.a10miaomiao.bilimiao.comm.store.FilterStore
+import com.kongzue.dialogx.dialogs.PopTip
+import kotlinx.coroutines.launch
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+import org.kodein.di.instance
+
+
+internal class FilterTagListPageViewModel(
+    override val di: DI,
+) : ViewModel(), DIAware {
+
+    private val fragment by instance<Fragment>()
+
+    private val filterStore by instance<FilterStore>()
+
+    val stateFlow get() = filterStore.stateFlow
+
+    fun addTag(text: String) {
+        val filterTagList = stateFlow.value.filterTagList
+        if (filterTagList.indexOf(text) == -1) {
+            filterStore.addTag(text)
+        } else {
+            PopTip.show("该标签已存在")
+        }
+    }
+
+    fun setTag(old: String, new: String) {
+        filterStore.setTag(old, new)
+    }
+
+    fun deleteSelected(selectedMap: Map<String, Int>) {
+        val tagList = selectedMap.keys.toList()
+        if (tagList.isEmpty()) {
+            PopTip.show("未选择标签")
+        }
+        filterStore.deleteTag(tagList)
+    }
+}
+
+@Composable
+internal fun FilterTagListPage() {
+    val viewModel: FilterTagListPageViewModel = diViewModel()
+
+    val state by viewModel.stateFlow.collectAsState()
+    val filterTagList = state.filterTagList
+
+    val selectedMap = remember {
+        mutableStateMapOf<String, Int>()
+    }
+
+    // -2为隐藏,-1为添加,>=0为编辑
+    var inputMode by remember {
+        mutableIntStateOf(-2)
+    }
+    var inputText by remember {
+        mutableStateOf("")
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(10.dp))
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                if (filterTagList.size > 0
+                    && selectedMap.size == filterTagList.size) {
+                    TextButton(onClick = {
+                        selectedMap.clear()
+                    }) {
+                        Text(text = "取消全选")
+                    }
+                } else {
+                    TextButton(
+                        onClick = {
+                            selectedMap.putAll(filterTagList.mapIndexed { index, s ->
+                                s to index
+                            })
+                        },
+                        enabled = filterTagList.size > 0,
+                    ) {
+                        Text(text = "全选")
+                    }
+                }
+
+                TextButton(
+                    onClick = {
+                        viewModel.deleteSelected(selectedMap.toMap())
+                        selectedMap.clear()
+                    },
+                    colors = ButtonDefaults.textButtonColors(
+                        contentColor = MaterialTheme.colorScheme.error,
+                    ),
+                    enabled = selectedMap.size > 0
+                ) {
+                    Text(text = "删除选中")
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Button(onClick = {
+                    inputMode = -1
+                }) {
+                    Text(text = "添加")
+                }
+            }
+
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+            ) {
+                items(filterTagList.size, { filterTagList[it] }) { index ->
+                    val tag = filterTagList[index]
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                inputMode = index
+                                inputText = tag
+                            }
+                    ) {
+                        Checkbox(
+                            checked = selectedMap.contains(tag),
+                            onCheckedChange = {
+                                if (selectedMap.contains(tag)) {
+                                    selectedMap.remove(tag)
+                                } else {
+                                    selectedMap[tag] = index
+                                }
+                            }
+                        )
+                        Text(
+                            text = tag,
+                            color = MaterialTheme.colorScheme.onSurface,
+                        )
+                    }
+                }
+
+                if (filterTagList.size == 0) {
+                    item {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .height(400.dp),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            Text(
+                                text = "空空如也\n去添加标签吧\n屏蔽标签会导致加载变慢!",
+                                color = MaterialTheme.colorScheme.outline,
+                                textAlign = TextAlign.Center,
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var errorText by remember {
+        mutableStateOf("")
+    }
+    val focusRequester = remember { FocusRequester() }
+    LaunchedEffect(inputMode) {
+        if (inputMode > -2) {
+            launch {
+                focusRequester.requestFocus()
+            }
+        }
+
+    }
+    fun handleDismiss() {
+        inputMode = -2
+        inputText = ""
+        errorText = ""
+    }
+
+    fun handleConfirm() {
+        if (inputText.isBlank()) {
+            errorText = "请输入标签"
+            return
+        }
+        if (inputMode < 0) {
+            viewModel.addTag(inputText)
+        } else {
+            val oldWord = filterTagList[inputMode]
+            viewModel.setTag(oldWord, inputText)
+        }
+        handleDismiss()
+    }
+
+    if (inputMode > -2) {
+        AlertDialog(
+            onDismissRequest = ::handleDismiss,
+            title = {
+                if (inputMode < 0) {
+                    Text(text = "添加屏蔽标签")
+                } else {
+                    Text(text = "编辑屏蔽标签")
+                }
+            },
+            text = {
+                Column {
+                    TextField(
+                        label = {
+                            Text(text = "标签")
+                        },
+                        isError = errorText.isNotBlank(),
+                        value = inputText,
+                        onValueChange = {
+                            inputText = it
+                            errorText = ""
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .focusRequester(focusRequester),
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                        keyboardActions = KeyboardActions(
+                            onDone = { handleConfirm() }
+                        ),
+                    )
+                }
+
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = ::handleConfirm,
+                ) {
+                    Text("确认添加")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = ::handleDismiss,
+                ) {
+                    Text("取消")
+                }
+            }
+        )
+    }
+
+}
