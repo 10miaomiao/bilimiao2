0b3964a8b531a2094bcebe0c5f5a3b7706643430
2894638479 <2894638479@qq.com>
2024-04-27 20:17:13 +0800

    支持酷狗歌词

---
+++ bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/pages/lyric/lib/ZLibUtils.java
@@ -0,0 +1,96 @@
+package cn.a10miaomiao.bilimiao.compose.pages.lyric.lib;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+
+
+//https://blog.csdn.net/qingzi635533/article/details/30231733
+public abstract class ZLibUtils
+{
+    public static byte[] compress(byte[] data) {
+        byte[] output = new byte[0];
+        Deflater compresser = new Deflater();
+        compresser.reset();
+        compresser.setInput(data);
+        compresser.finish();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(data.length);
+        try {
+            byte[] buf = new byte[1024];
+            while (!compresser.finished()) {
+                int i = compresser.deflate(buf);
+                bos.write(buf, 0, i);
+            }
+            output = bos.toByteArray();
+        } catch (Exception e) {
+            output = data;
+            e.printStackTrace();
+        } finally {
+            try {
+                bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        compresser.end();
+        return output;
+    }
+
+    public static void compress(byte[] data, OutputStream os) {
+        DeflaterOutputStream dos = new DeflaterOutputStream(os);
+        try {
+            dos.write(data, 0, data.length);
+            dos.finish();
+            dos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static byte[] decompress(byte[] data) {
+        byte[] output = new byte[0];
+        Inflater decompresser = new Inflater();
+        decompresser.reset();
+        decompresser.setInput(data);
+        ByteArrayOutputStream o = new ByteArrayOutputStream(data.length);
+        try {
+            byte[] buf = new byte[1024];
+            while (!decompresser.finished()) {
+                int i = decompresser.inflate(buf);
+                o.write(buf, 0, i);
+            }
+            output = o.toByteArray();
+        } catch (Exception e) {
+            output = data;
+            e.printStackTrace();
+        } finally {
+            try {
+                o.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        decompresser.end();
+        return output;
+    }
+
+    public static byte[] decompress(InputStream is) {
+        InflaterInputStream iis = new InflaterInputStream(is);
+        ByteArrayOutputStream o = new ByteArrayOutputStream(1024);
+        try {
+            int i = 1024;
+            byte[] buf = new byte[i];
+            while ((i = iis.read(buf, 0, i)) > 0) {
+                o.write(buf, 0, i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return o.toByteArray();
+    }
+}
