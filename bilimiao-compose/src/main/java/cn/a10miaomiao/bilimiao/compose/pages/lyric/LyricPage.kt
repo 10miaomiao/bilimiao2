ab804e7d6b9c703b66f8afc9d7ad839e4e753498
2894638479 <2894638479@qq.com>
2024-04-28 18:31:43 +0800

    歌词界面微调

--- bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/pages/lyric/LyricPage.kt
+++ bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/pages/lyric/LyricPage.kt
@@ -1,19 +1,15 @@
 package cn.a10miaomiao.bilimiao.compose.pages.lyric
 
 import android.app.Activity
-import android.util.Log
 import android.view.View
-import android.view.Window
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -29,13 +25,11 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
@@ -62,17 +56,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 import org.kodein.di.compose.rememberInstance
 import org.kodein.di.instance
-import java.io.ByteArrayOutputStream
 import java.util.Base64
-import java.util.zip.Inflater
-import kotlin.experimental.xor
 
 class LyricPage :ComposePage(){
     override val route: String
@@ -402,7 +391,7 @@ internal fun LyricPageContent(viewModel: LyricPageViewModel){
     val scrollState= rememberLazyListState()
     val conf = LocalConfiguration.current
     var canvasHeight by remember { mutableFloatStateOf(0f) }
-    val spacerPadding = remember(canvasHeight,windowState.contentInsets.bottom){
+    val spacerHeight = remember(canvasHeight,windowState.contentInsets.bottom){
         derivedStateOf {
             ((canvasHeight-windowState.contentInsets.bottom)/2*160/conf.densityDpi).toInt()-50
         }
@@ -417,18 +406,16 @@ internal fun LyricPageContent(viewModel: LyricPageViewModel){
             viewModel.loadSource(videoTitle)
         }
     }
-    LaunchedEffect(key1 = focusOn){
-        scrollState.animateScrollToItem(focusOn+1,-spacerPadding.value*conf.densityDpi/160)
+    LaunchedEffect(key1 = focusOn,spacerHeight){
+        scrollState.animateScrollToItem(focusOn+1,-spacerHeight.value*conf.densityDpi/160)
     }
     Box{
         Canvas(modifier = Modifier.fillMaxSize()){
-            if(size.height!= 0f){
-                canvasHeight = size.height
-            }
+            canvasHeight = size.height
         }
         LazyColumn(state = scrollState) {
             item {
-                Spacer(modifier = Modifier.height(spacerPadding.value.dp))
+                Spacer(modifier = Modifier.height(spacerHeight.value.dp))
             }
             items(lyric) {
                 if (focusOn == lyric.indexOf(it)) {
@@ -440,7 +427,7 @@ internal fun LyricPageContent(viewModel: LyricPageViewModel){
                 }
             }
             item {
-                Spacer(modifier = Modifier.height(spacerPadding.value.dp))
+                Spacer(modifier = Modifier.height(spacerHeight.value.dp + windowState.contentInsets.bottomDp.dp))
             }
         }
     }
