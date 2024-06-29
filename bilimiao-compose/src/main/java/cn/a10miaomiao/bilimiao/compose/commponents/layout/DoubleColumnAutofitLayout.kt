7af6ca1c19347be9ae7b10d151cac41a9f342695
10miaomiao <1033607264@qq.com>
2024-06-29 23:06:11 +0800

    修复自动双列布局垂直滚动问题

--- bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/commponents/layout/DoubleColumnAutofitLayout.kt
+++ bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/commponents/layout/DoubleColumnAutofitLayout.kt
@@ -8,16 +8,46 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import cn.a10miaomiao.bilimiao.compose.commponents.layout.chain_scrollable.ChainScrollableLayout
+import cn.a10miaomiao.bilimiao.compose.commponents.layout.chain_scrollable.ChainScrollableLayoutNestedScrollConnection
 import cn.a10miaomiao.bilimiao.compose.commponents.layout.chain_scrollable.ChainScrollableLayoutState
+import com.a10miaomiao.bilimiao.comm.utils.miaoLogger
 import kotlin.math.roundToInt
 
+internal class DoubleColumnAutofitLayoutTopNestedScrollConnection(
+    val state: ChainScrollableLayoutState,
+) : NestedScrollConnection {
+    override fun onPreScroll(
+        available: Offset,
+        source: NestedScrollSource
+    ): Offset {
+        if (
+            available.y > 0
+            && state.getOffsetYValue() < 0
+        ) {
+            state.setOffsetY(minOf(state.getOffsetYValue() + available.y, 0f))
+            return available
+        }
+        return Offset.Zero
+    }
 
+    override fun onPostScroll(
+        consumed: Offset,
+        available: Offset,
+        source: NestedScrollSource
+    ): Offset {
+        return super.onPostScroll(consumed, available, source)
+    }
+}
 
 @Composable
 fun DoubleColumnAutofitLayout(
@@ -26,8 +56,8 @@ fun DoubleColumnAutofitLayout(
     leftMaxHeight: Dp,
     leftMaxWidth: Dp,
     chainScrollableLayoutState: ChainScrollableLayoutState,
-    leftContent: @Composable BoxScope.(orientation: Orientation) -> Unit,
-    content: @Composable BoxScope.(orientation: Orientation) -> Unit,
+    leftContent: @Composable BoxScope.(Orientation, PaddingValues) -> Unit,
+    content: @Composable BoxScope.(Orientation, PaddingValues) -> Unit,
 ) {
     BoxWithConstraints(
         modifier = modifier
@@ -35,26 +65,28 @@ fun DoubleColumnAutofitLayout(
         if (maxWidth > leftMaxWidth) {
             Row() {
                 Box(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(
-                            start = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
-                            end = 5.dp,
+                    modifier = Modifier.weight(1f)
+                ) {
+                    leftContent(
+                        Orientation.Horizontal,
+                        PaddingValues.Absolute(
+                            left = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
                             top = innerPadding.calculateTopPadding(),
                             bottom = innerPadding.calculateBottomPadding(),
-                        ),
-                ) {
-                    leftContent(Orientation.Horizontal)
+                        )
+                    )
                 }
                 Box(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(
-                            start = 0.dp,
-                            end = innerPadding.calculateEndPadding(LayoutDirection.Ltr),
-                        )
+                    modifier = Modifier.weight(1f)
                 ) {
-                    content(Orientation.Horizontal)
+                    content(
+                        Orientation.Horizontal,
+                        PaddingValues.Absolute(
+                            top = innerPadding.calculateTopPadding(),
+                            bottom = innerPadding.calculateBottomPadding(),
+                            right = innerPadding.calculateEndPadding(LayoutDirection.Ltr),
+                        )
+                    )
                 }
             }
         } else {
@@ -63,6 +95,9 @@ fun DoubleColumnAutofitLayout(
                 density.run { leftMaxHeight.roundToPx().toFloat() }
             }
             val scrollableState = rememberScrollState()
+            val nestedScrollState = remember(chainScrollableLayoutState) {
+                DoubleColumnAutofitLayoutTopNestedScrollConnection(chainScrollableLayoutState)
+            }
             ChainScrollableLayout(
                 modifier = modifier,
                 state = chainScrollableLayoutState,
@@ -71,25 +106,26 @@ fun DoubleColumnAutofitLayout(
                 Box(
                     modifier = Modifier
                         .height(leftMaxHeight)
-                        .padding(
-                            start = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
-                            end = innerPadding.calculateEndPadding(LayoutDirection.Ltr),
-                            top = innerPadding.calculateTopPadding(),
-                            bottom = 0.dp,
-                        )
                         .offset {
                             IntOffset(
                                 0,
-//                                    (state.getOffsetYValue() / 2).roundToInt()
                                 state
                                     .getOffsetYValue()
                                     .roundToInt()
                             )
                         }
                         .alpha(alpha)
+                        .nestedScroll(nestedScrollState)
                         .scrollable(scrollableState, Orientation.Vertical),
                 ) {
-                    leftContent(Orientation.Vertical)
+                    leftContent(
+                        Orientation.Vertical,
+                        PaddingValues.Absolute(
+                            top = innerPadding.calculateTopPadding(),
+                            left = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
+                            right = innerPadding.calculateEndPadding(LayoutDirection.Ltr),
+                        )
+                    )
                 }
                 Box(
                     modifier = Modifier
@@ -99,12 +135,16 @@ fun DoubleColumnAutofitLayout(
                                 (state.maxPx + state.getOffsetYValue()).roundToInt()
                             )
                         }
-                        .padding(
-                            start = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
-                            end = innerPadding.calculateEndPadding(LayoutDirection.Ltr),
-                        )
                 ) {
-                    content(Orientation.Vertical)
+                    content(
+                        Orientation.Vertical,
+                        PaddingValues.Absolute(
+                            bottom = innerPadding.calculateBottomPadding(),
+                            left = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
+                            right = innerPadding.calculateEndPadding(LayoutDirection.Ltr),
+//                            top = density.run { (state.maxPx + state.getOffsetYValue()).toDp() }
+                        )
+                    )
                 }
             }
         }
