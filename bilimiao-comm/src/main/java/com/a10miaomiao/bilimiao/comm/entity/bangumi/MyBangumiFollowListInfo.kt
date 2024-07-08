6f46d17e21c984141e417236e5d9048b49f5667b
10miaomiao <1033607264@qq.com>
2024-07-08 21:12:36 +0800

    修复 我的追番列表为空时闪退BUG

--- bilimiao-comm/src/main/java/com/a10miaomiao/bilimiao/comm/entity/bangumi/MyBangumiFollowListInfo.kt
+++ bilimiao-comm/src/main/java/com/a10miaomiao/bilimiao/comm/entity/bangumi/MyBangumiFollowListInfo.kt
@@ -1,7 +1,7 @@
 package com.a10miaomiao.bilimiao.comm.entity.bangumi
 
 data class MyBangumiFollowListInfo(
-    val follow_list: List<MyBangumiInfo>,
+    val follow_list: List<MyBangumiInfo>?,
     val has_next: Int,
     val total: Int,
 )
