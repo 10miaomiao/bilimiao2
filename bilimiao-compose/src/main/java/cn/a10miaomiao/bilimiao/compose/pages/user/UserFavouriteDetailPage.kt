c645284929e452a09578a5ca1b401a78f56f00b4
10miaomiao <1033607264@qq.com>
2024-11-18 21:10:44 +0800

    修复收藏详情列表搜索闪退

--- bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/pages/user/UserFavouriteDetailPage.kt
+++ bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/pages/user/UserFavouriteDetailPage.kt
@@ -3,6 +3,7 @@ package cn.a10miaomiao.bilimiao.compose.pages.user
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
+import bilibili.polymer.app.search.v1.Item
 import cn.a10miaomiao.bilimiao.compose.base.ComposePage
 import cn.a10miaomiao.bilimiao.compose.base.stringPageArg
 import cn.a10miaomiao.bilimiao.compose.pages.user.content.UserFavouriteDetailContent
@@ -11,15 +12,18 @@ class UserFavouriteDetailPage : ComposePage() {
 
     val id = stringPageArg("id")
     val title = stringPageArg("title")
+    val keyword = stringPageArg("keyword", "")
+
 
     override val route: String
-        get() = "user/favourite/${id}?title=${title}"
+        get() = "user/favourite/${id}?title=${title}&keyword=${keyword}"
 
     @Composable
     override fun AnimatedContentScope.Content(navEntry: NavBackStackEntry) {
         val mediaId = navEntry.arguments?.get(id) ?: ""
         val mediaTitle = navEntry.arguments?.get(title) ?: ""
-        UserFavouriteDetailPageContent(mediaId, mediaTitle)
+        val keyword = navEntry.arguments?.get(keyword) ?: ""
+        UserFavouriteDetailPageContent(mediaId, mediaTitle, keyword)
     }
 }
 
@@ -27,11 +31,12 @@ class UserFavouriteDetailPage : ComposePage() {
 private fun UserFavouriteDetailPageContent(
     mediaId: String,
     mediaTitle: String,
+    keyword: String,
 ) {
-
     UserFavouriteDetailContent(
         mediaId = mediaId,
         mediaTitle = mediaTitle,
+        keyword = keyword,
         showTowPane = false,
         hideFirstPane = false,
         onChangeHideFirstPane = {},
