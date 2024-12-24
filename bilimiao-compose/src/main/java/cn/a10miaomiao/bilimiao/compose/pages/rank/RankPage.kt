b8189cc273866c8ae722e29f7e1710fcf263bdf1
10miaomiao <1033607264@qq.com>
2024-12-24 23:03:39 +0800

    增加新排行榜(compose)页面

---
+++ bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/pages/rank/RankPage.kt
@@ -0,0 +1,172 @@
+package cn.a10miaomiao.bilimiao.compose.pages.rank
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cn.a10miaomiao.bilimiao.compose.base.ComposePage
+import cn.a10miaomiao.bilimiao.compose.common.diViewModel
+import cn.a10miaomiao.bilimiao.compose.common.flow.stateMap
+import cn.a10miaomiao.bilimiao.compose.common.foundation.pagerTabIndicatorOffset
+import cn.a10miaomiao.bilimiao.compose.common.localContainerView
+import cn.a10miaomiao.bilimiao.compose.common.mypage.PageConfig
+import cn.a10miaomiao.bilimiao.compose.components.list.ListStateBox
+import cn.a10miaomiao.bilimiao.compose.pages.rank.content.RankListContent
+import cn.a10miaomiao.bilimiao.compose.pages.user.content.TagFollowContent
+import com.a10miaomiao.bilimiao.comm.store.RegionStore
+import com.a10miaomiao.bilimiao.store.WindowStore
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+import org.kodein.di.compose.rememberInstance
+import org.kodein.di.instance
+
+
+@Serializable
+class RankPage : ComposePage() {
+
+    @Composable
+    override fun Content() {
+        val viewModel: RankPageViewModel = diViewModel()
+        RankPageContent(viewModel)
+    }
+}
+
+private class RankPageViewModel(
+    override val di: DI,
+) : ViewModel(), DIAware {
+
+    private val fragment by instance<Fragment>()
+    private val regionStore: RegionStore by instance()
+
+    val regionNames = mutableStateListOf<String>()
+    val regionIds = mutableStateListOf<Int>()
+
+    init {
+        viewModelScope.launch {
+            regionStore.stateFlow.collect {
+                regionNames.clear()
+                regionIds.clear()
+                regionNames.add("全站")
+                regionIds.add(0)
+                it.regions.forEach { region ->
+                    regionNames.add(region.name)
+                    regionIds.add(region.tid)
+                }
+            }
+        }
+    }
+
+}
+
+
+@Composable
+private fun RankPageContent(
+    viewModel: RankPageViewModel
+) {
+    PageConfig(
+        title = "排行榜"
+    )
+
+    val windowStore: WindowStore by rememberInstance()
+    val windowState = windowStore.stateFlow.collectAsState().value
+    val windowInsets = windowState.getContentInsets(localContainerView())
+
+    val pagerState = rememberPagerState { viewModel.regionIds.size }
+    val currentPage = pagerState.currentPage
+    val scope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            ScrollableTabRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(
+                        top = windowInsets.topDp.dp,
+                        start = windowInsets.leftDp.dp,
+                        end = windowInsets.rightDp.dp,
+                    ),
+                edgePadding = 0.dp,
+                selectedTabIndex = currentPage,
+                indicator = { positions ->
+                    if (currentPage > -1 && currentPage < positions.size) {
+                        TabRowDefaults.PrimaryIndicator(
+                            Modifier.pagerTabIndicatorOffset(pagerState, positions),
+                        )
+                    }
+                },
+                divider = {}
+            ) {
+                viewModel.regionIds.forEachIndexed { index, region ->
+                    Tab(
+                        text = {
+                            Text(
+                                text = viewModel.regionNames[index],
+                                color = if (index == pagerState.currentPage) {
+                                    MaterialTheme.colorScheme.primary
+                                } else {
+                                    MaterialTheme.colorScheme.onBackground
+                                }
+                            )
+                        },
+                        selected = pagerState.currentPage == index,
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+                        },
+                    )
+                }
+            }
+            HorizontalDivider(
+                Modifier
+                    .align(Alignment.BottomStart)
+                    .fillMaxWidth()
+            )
+        }
+
+        val saveableStateHolder = rememberSaveableStateHolder()
+        HorizontalPager(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            state = pagerState,
+        ) { index ->
+            val id = viewModel.regionIds[index]
+            saveableStateHolder.SaveableStateProvider(id.toString()) {
+                RankListContent(
+                    regionId = id,
+                )
+            }
+        }
+
+    }
+}
---
+++ bilimiao-compose/src/main/java/cn/a10miaomiao/bilimiao/compose/pages/rank/content/RankListContent.kt
@@ -0,0 +1,261 @@
+package cn.a10miaomiao.bilimiao.compose.pages.rank.content
+
+import android.app.Activity
+import android.net.Uri
+import android.view.Gravity
+import android.widget.TextView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.fragment.findNavController
+import bilibili.app.show.v1.RankAllResultReq
+import bilibili.app.show.v1.RankGRPC
+import bilibili.app.show.v1.RankRegionResultReq
+import cn.a10miaomiao.bilimiao.compose.common.defaultNavOptions
+import cn.a10miaomiao.bilimiao.compose.common.diViewModel
+import cn.a10miaomiao.bilimiao.compose.common.entity.FlowPaginationInfo
+import cn.a10miaomiao.bilimiao.compose.common.localContainerView
+import cn.a10miaomiao.bilimiao.compose.common.navigation.findComposeNavController
+import cn.a10miaomiao.bilimiao.compose.components.list.ListStateBox
+import cn.a10miaomiao.bilimiao.compose.components.list.SwipeToRefresh
+import cn.a10miaomiao.bilimiao.compose.components.video.VideoItemBox
+import cn.a10miaomiao.bilimiao.compose.pages.user.FollowingItemInfo
+import cn.a10miaomiao.bilimiao.compose.pages.user.FollowingListAction
+import cn.a10miaomiao.bilimiao.compose.pages.user.InterrelationInfo
+import cn.a10miaomiao.bilimiao.compose.pages.user.MyFollowViewModel
+import cn.a10miaomiao.bilimiao.compose.pages.user.UserSpacePage
+import cn.a10miaomiao.bilimiao.compose.pages.user.UserTagSetDialogState
+import cn.a10miaomiao.bilimiao.compose.pages.user.components.UserInfoCard
+import com.a10miaomiao.bilimiao.comm.entity.MessageInfo
+import com.a10miaomiao.bilimiao.comm.entity.ResultInfo
+import com.a10miaomiao.bilimiao.comm.network.BiliApiService
+import com.a10miaomiao.bilimiao.comm.network.BiliGRPCHttp
+import com.a10miaomiao.bilimiao.comm.network.MiaoHttp.Companion.gson
+import com.a10miaomiao.bilimiao.comm.network.MiaoHttp.Companion.string
+import com.a10miaomiao.bilimiao.comm.store.UserStore
+import com.a10miaomiao.bilimiao.store.WindowStore
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.google.gson.JsonSyntaxException
+import com.kongzue.dialogx.dialogs.PopTip
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+import org.kodein.di.compose.localDI
+import org.kodein.di.compose.rememberInstance
+import org.kodein.di.instance
+
+private class RankListContentViewModel(
+    override val di: DI,
+    val regionId: Int,
+) : ViewModel(), DIAware {
+
+    private val activity by instance<Activity>()
+    private val fragment by instance<Fragment>()
+    private val userStore by instance<UserStore>()
+
+    val count = MutableStateFlow(1)
+    val isRefreshing = MutableStateFlow(false)
+    val list = FlowPaginationInfo<bilibili.app.show.v1.Item>()
+
+    init {
+        loadData()
+    }
+
+
+    fun loadData(
+        pageNum: Int = list.pageNum
+    ) = viewModelScope.launch(Dispatchers.IO) {
+        try {
+            val result = BiliGRPCHttp.request {
+                if (regionId == 0) {
+                    val req = RankAllResultReq(
+                        order = "all",
+                        pn = pageNum,
+                        ps = list.pageSize
+                    )
+                    RankGRPC.rankAll(req)
+                } else {
+                    val req = RankRegionResultReq(
+                        rid = regionId,
+                        pn = pageNum,
+                        ps = list.pageSize
+                    )
+                    RankGRPC.rankRegion(req)
+                }
+            }.awaitCall()
+//            var totalCount = 0 // 屏蔽前数量
+//            if (result.size < list.pageSize) {
+//                ui.setState { list.finished = true }
+//            }
+//            totalCount = result.size
+//            result = result.filter {
+//                filterStore.filterWord(it.title)
+//                        && filterStore.filterUpper(it.mid.toLong())
+//            }
+            if (pageNum == 1) {
+                list.data.value = result.items
+            } else {
+                list.data.value = list.data.value
+                    .toMutableList()
+                    .also { it.addAll(result.items) }
+            }
+            list.pageNum = pageNum
+//            if (list.data.size < 10 && totalCount != result.size) {
+//                _loadData(pageNum + 1)
+//            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            list.fail.value = "无法连接到御坂网络"
+        } finally {
+            list.loading.value = false
+            isRefreshing.value = false
+        }
+    }
+
+    fun loadMore() {
+        if (!list.finished.value && !list.loading.value) {
+            loadData(list.pageNum + 1)
+        }
+    }
+
+    fun refresh(
+        refreshing: Boolean = true
+    ) {
+        isRefreshing.value = refreshing
+        list.finished.value = false
+        list.fail.value = ""
+        loadData(1)
+    }
+
+    fun toVideoDetail(item: bilibili.app.show.v1.Item) {
+        fragment.findNavController()
+            .navigate(
+                Uri.parse("bilimiao://video/" + item.param),
+                defaultNavOptions,
+            )
+    }
+}
+
+@Composable
+internal fun RankListContent(
+    regionId: Int,
+) {
+    val viewModel = diViewModel(key = regionId.toString()) {
+        RankListContentViewModel(it, regionId)
+    }
+    val myFollowViewModel: MyFollowViewModel by rememberInstance()
+    val windowStore: WindowStore by rememberInstance()
+    val userStore: UserStore by rememberInstance()
+    val windowState = windowStore.stateFlow.collectAsState().value
+    val windowInsets = windowState.getContentInsets(localContainerView())
+
+    val list by viewModel.list.data.collectAsState()
+    val listLoading by viewModel.list.loading.collectAsState()
+    val listFinished by viewModel.list.finished.collectAsState()
+    val listFail by viewModel.list.fail.collectAsState()
+    val isRefreshing by viewModel.isRefreshing.collectAsState()
+    val isLogin = userStore.isLogin()
+
+    SwipeToRefresh(
+        refreshing = isRefreshing,
+        onRefresh = { viewModel.refresh() },
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(330.dp),
+            modifier = Modifier.padding(
+                start = windowInsets.leftDp.dp,
+                end = windowInsets.rightDp.dp,
+            )
+        ) {
+            items(list.size, { list[it].param }) {
+                val item = list[it]
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(
+                            end = 10.dp,
+                            top = 5.dp,
+                            bottom = 5.dp,
+                        ),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        modifier = Modifier.width(30.dp),
+                        text = (it + 1).toString(),
+                        textAlign = TextAlign.Center,
+                        color = if (it > 2) {
+                            MaterialTheme.colorScheme.onBackground
+                        } else {
+                            MaterialTheme.colorScheme.primary
+                        }
+                    )
+                    VideoItemBox(
+                        modifier = Modifier.weight(1f),
+                        title = item.title,
+                        pic = item.cover,
+                        upperName = item.name,
+                        playNum = item.play.toString(),
+                        damukuNum = item.danmaku.toString(),
+                        onClick = {
+                            viewModel.toVideoDetail(item)
+                        }
+                    )
+                }
+            }
+
+            item(
+                span = { GridItemSpan(maxLineSpan) }
+            ) {
+                ListStateBox(
+                    modifier = Modifier.padding(
+                        bottom = windowInsets.bottomDp.dp
+                    ),
+                    loading = listLoading,
+                    finished = listFinished,
+                    fail = listFail,
+                    listData = list,
+                ) {
+                    viewModel.loadMore()
+                }
+            }
+        }
+    }
+}
