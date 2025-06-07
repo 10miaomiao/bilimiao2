9cccd3e387fc3ee418b1cbc838a39292ae466de8
10miaomiao <1033607264@qq.com>
2025-06-07 20:37:02 +0800

    修复字幕json解析错误

--- bilimiao-comm/src/main/java/com/a10miaomiao/bilimiao/comm/entity/player/SubtitleJsonInfo.kt
+++ bilimiao-comm/src/main/java/com/a10miaomiao/bilimiao/comm/entity/player/SubtitleJsonInfo.kt
@@ -4,10 +4,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SubtitleJsonInfo(
-    val font_size: Float,
-    val font_color: String,
-    val background_alpha: Float,
-    val background_color: String,
+//    val font_size: Float = 1f,
+//    val font_color: String = "#fff",
+//    val background_alpha: Float = 1f,
+//    val background_color: String = "#000",
     val Stroke: String = "",
     val body: List<ItemInfo>,
 ) {
