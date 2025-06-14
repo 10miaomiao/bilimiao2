a17ddfbea2728f17799f5f94ee54becd1ef18cc3
10miaomiao <1033607264@qq.com>
2025-06-14 17:44:46 +0800

    增加Desugaring脱糖库，解决低版本安卓kotlinx-datetime库无法使用问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,9 @@ android {
     }
 
     compileOptions {
+        // Flag to enable support for the new language APIs
+        isCoreLibraryDesugaringEnabled = true
+
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -105,6 +108,8 @@ android {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+
     implementation(Libraries.core)
     implementation(Libraries.appcompat)
     implementation(Libraries.material)
