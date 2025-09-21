ff0278d537c5f589515806dad02b585229a94872
10miaomiao <1033607264@qq.com>
2025-09-21 13:45:12 +0800

    播放器界面按钮适配新版主题色

--- app/src/main/java/com/a10miaomiao/bilimiao/comm/delegate/player/AreaLimitBoxController.kt
+++ app/src/main/java/com/a10miaomiao/bilimiao/comm/delegate/player/AreaLimitBoxController.kt
@@ -1,11 +1,14 @@
 package com.a10miaomiao.bilimiao.comm.delegate.player
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import cn.a10miaomiao.bilimiao.compose.pages.setting.proxy.SelectProxyServerPage
 import com.a10miaomiao.bilimiao.R
 import com.a10miaomiao.bilimiao.comm.navigation.openBottomSheet
+import com.google.android.material.button.MaterialButton
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
@@ -16,8 +19,8 @@ class AreaLimitBoxController(
 ) : DIAware {
 
     val areaLimitLayout = activity.findViewById<RelativeLayout>(R.id.area_limit_layout)
-    val areaLimitOkBtn = activity.findViewById<View>(R.id.area_limit_ok_btn)
-    val areaLimitCloseBtn = activity.findViewById<View>(R.id.area_limit_close_btn)
+    val areaLimitOkBtn = activity.findViewById<MaterialButton>(R.id.area_limit_ok_btn)
+    val areaLimitCloseBtn = activity.findViewById<MaterialButton>(R.id.area_limit_close_btn)
 
     init {
         initAreaLimitBox()
@@ -47,6 +50,14 @@ class AreaLimitBoxController(
     fun hide() {
         areaLimitLayout.visibility = View.GONE
         delegate.views.videoPlayer.visibility = View.VISIBLE
+    }
+
+    fun updateThemeColor(themeColor: Int) {
+        areaLimitCloseBtn.setTextColor(themeColor)
+        areaLimitCloseBtn.backgroundTintList = ColorStateList.valueOf(Color.TRANSPARENT)
+        areaLimitCloseBtn.rippleColor = ColorStateList.valueOf(0x66FFFFFF.toInt())
+        areaLimitOkBtn.backgroundTintList = ColorStateList.valueOf(themeColor)
+        areaLimitOkBtn.rippleColor = ColorStateList.valueOf(0x33000000)
     }
 
 }
--- app/src/main/java/com/a10miaomiao/bilimiao/comm/delegate/player/CompletionBoxController.kt
+++ app/src/main/java/com/a10miaomiao/bilimiao/comm/delegate/player/CompletionBoxController.kt
@@ -1,10 +1,14 @@
 package com.a10miaomiao.bilimiao.comm.delegate.player
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.View
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import androidx.appcompat.app.AppCompatActivity
 import com.a10miaomiao.bilimiao.R
+import com.google.android.material.button.MaterialButton
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
@@ -17,8 +21,8 @@ class CompletionBoxController(
     val completionLayout = activity.findViewById<RelativeLayout>(R.id.completion_layout)
     val completionText = activity.findViewById<TextView>(R.id.completion_text)
     val completionTextHold = activity.findViewById<TextView>(R.id.completion_text_hold)
-    val completionRetryBtn = activity.findViewById<View>(R.id.completion_retry_btn)
-    val completionCloseBtn = activity.findViewById<View>(R.id.completion_close_btn)
+    val completionRetryBtn = activity.findViewById<MaterialButton>(R.id.completion_retry_btn)
+    val completionCloseBtn = activity.findViewById<MaterialButton>(R.id.completion_close_btn)
 
     init {
         initCompletionBox()
@@ -65,4 +69,13 @@ class CompletionBoxController(
             completionTextHold.visibility = View.GONE
         }
     }
+
+    fun updateThemeColor(themeColor: Int) {
+        completionCloseBtn.setTextColor(themeColor)
+        completionCloseBtn.backgroundTintList = ColorStateList.valueOf(Color.TRANSPARENT)
+        completionCloseBtn.rippleColor = ColorStateList.valueOf(0x66FFFFFF.toInt())
+        completionRetryBtn.backgroundTintList = ColorStateList.valueOf(themeColor)
+        completionRetryBtn.rippleColor = ColorStateList.valueOf(0x33000000)
+    }
+
 }
