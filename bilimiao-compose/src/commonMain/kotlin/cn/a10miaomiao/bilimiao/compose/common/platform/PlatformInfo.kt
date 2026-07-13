56910001fd53db2d4e75575731223cd243aecec4
10miaomiao <1033607264@qq.com>
2026-07-14 00:13:47 +0800

    feat-platform-enum-hide-scanner-on-desktop

--- bilimiao-compose/src/commonMain/kotlin/cn/a10miaomiao/bilimiao/compose/common/platform/PlatformInfo.kt
+++ bilimiao-compose/src/commonMain/kotlin/cn/a10miaomiao/bilimiao/compose/common/platform/PlatformInfo.kt
@@ -2,8 +2,11 @@ package cn.a10miaomiao.bilimiao.compose.common.platform
 
 expect val platformInfo: PlatformInfo
 
+enum class Platform { ANDROID, DESKTOP, IOS, WEB }
+
 interface PlatformInfo {
     val model: String
     val osVersion: String
     val sdkInt: Int
+    val platform: Platform
 }
--- bilimiao-compose/src/desktopMain/kotlin/cn/a10miaomiao/bilimiao/compose/common/platform/PlatformInfoDesktop.kt
+++ bilimiao-compose/src/desktopMain/kotlin/cn/a10miaomiao/bilimiao/compose/common/platform/PlatformInfoDesktop.kt
@@ -4,4 +4,5 @@ actual val platformInfo: PlatformInfo = object : PlatformInfo {
     override val model: String = System.getProperty("os.name") ?: "Desktop"
     override val osVersion: String = System.getProperty("os.version") ?: "0"
     override val sdkInt: Int = 0
+    override val platform: Platform = Platform.DESKTOP
 }
